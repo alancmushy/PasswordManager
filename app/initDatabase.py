68773810582f66e83804859f859f312d65a795d3
Alan Mushyirahamwe <alancmushy@gmail.com>
2025-07-12 17:44:10 -0400

    Retooled data collection

--- app/initDatabase.py
+++ app/initDatabase.py
@@ -1,7 +1,6 @@
 import sqlite3
-import os
 def startup():
-    os.makedirs("data", exist_ok=True)
+    
     connection = sqlite3.connect('data/passwordDatabase.db')
 
     with open('app/passProtect.sql') as f:
