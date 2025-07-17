bbb9f4d45677d0531b6e0f951ea93ad8b4df6184
Alan Mushyirahamwe <alancmushy@gmail.com>
2025-07-17 11:38:16 -0400

    Debugging checking path

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -4,4 +4,6 @@ const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL||'http://localhost:8000',
 })
 
+console.log("NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
+
 export default api;
