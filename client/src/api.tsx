4863958f03657c693c972dd337676b5ccab76bd2
Alan Mushyirahamwe <alancmushy@gmail.com>
2025-07-17 22:30:36 -0400

    Fixing path

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    baseURL:'https://archlock.onrender.com'
 })
 
 console.log("NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
