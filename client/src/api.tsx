0ede073214dad937d3f61ed134f7bcd1bb465c1c
Alan Mushyirahamwe <alancmushy@gmail.com>
2025-07-17 22:27:24 -0400

    Debugging checking path

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL||'http://localhost:8000',
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 })
 
 console.log("NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
